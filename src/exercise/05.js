1b5b77d6df6618fc13c104b85c41673797258313
Anhnhi Tran <anhnhi.tran@parachutehealth.com>
2023-10-30 16:03:33 -0600

    Exercises 5-7

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,18 +14,73 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// Exercise 5
+// const smallBox = (
+//   <div
+//     className="box box--small"
+//     style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
+//   >
+//     small lightblue box
+//   </div>  
+// )
+
+// const mediumBox = (
+//   <div
+//     className="box box--medium"
+//     style={{fontStyle: 'italic', backgroundColor: 'pink'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+
+// const largeBox = (
+//   <div
+//     className="box box--large"
+//     style={{fontStyle: 'italic', backgroundColor: 'orange'}}
+//     >
+//       large orange box
+//   </div>
+// )
+
+// function App() {
+//   return (
+//     <div>
+//       {smallBox}
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+// export default App
+
+// Extra credit
+
+function Box({style, size, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+      <Box>sizeless box</Box>
     </div>
   )
 }
-
 export default App
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,28 +3,57 @@
 
 import * as React from 'react'
 
-function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+// 🐨 add a submit event handler here (`handleSubmit`).
+// 💰 Make sure to accept the `event` as an argument and call
+// `event.preventDefault()` to prevent the default behavior of form submit
+// events (which refreshes the page).
+// 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+//
+// 🐨 get the value from the username input (using whichever method
+// you prefer from the options mentioned in the instructions)
+// 💰 For example: event.target.elements[0].value
+// 🐨 Call `onSubmitUsername` with the value of the input
 
   // 🐨 add the onSubmit handler to the <form> below
-
+  
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  
+  function UsernameForm({onSubmitUsername}) {
+    // const usernameInputRef = React.useRef() // bonus 1
+    // const [error, setError] = React.useState(null) // bonus 2
+    
+    const [username, setUsername] = React.useState('')
+
+    function handleSubmit(event){
+      event.preventDefault()
+      onSubmitUsername(username)
+    }
+
+  function handleChange(event) {
+    setUsername(event.target.value.toLowerCase())
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input
+          id="usernameInput"
+          type="text"
+          onChange={handleChange}
+          value={username}
+        />
+        {/* bonus 2 */}
+        {/* <input id="usernameInput" type="text" onChange={handleChange} /> */}
+        {/* bonus 1 */}
+        {/* <input id="usernameInput" type="text" ref={usernameInputRef} />   */}
       </div>
+      {/* bonus 2 */}
+      {/* <div role="alert" style={{color: 'red'}}> 
+        {error}
+      </div> */}
+      {/* <button disabled={Boolean(error)} type="submit"> */}
       <button type="submit">Submit</button>
     </form>
   )
